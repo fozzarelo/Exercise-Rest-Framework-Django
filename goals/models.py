e7a55f993b1266d6455254dba3d0637d17c5bfdb
Dan Pinto <fozzarelo@gmail.com>
2016-07-31 11:39:22 -0700

    Add admin module and superuser

--- goals/models.py
+++ goals/models.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.db import models
 
 
-# Create your models here.
 class Goal(models.Model):
     name = models.CharField(max_length=250)
     description = models.TextField(null=True)
@@ -15,7 +14,5 @@ class Task(models.Model):
     name = models.CharField(max_length=250)
     completed = models.BooleanField(default=False)
     goal = models.ForeignKey(Goal, related_name="tasks", blank=True, null=True)
-
     created = models.DateTimeField(auto_now_add=True)
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, null=True)
-
